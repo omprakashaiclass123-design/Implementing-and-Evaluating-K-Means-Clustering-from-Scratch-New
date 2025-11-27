cceeb7fcc8b96fa07e012e1611111cd3afdb1870
omprakashaiclass123-design <omprakashaiclass123@gmail.com>
2025-11-27 00:29:32 -0800

    Add files via upload

--- kmeans_scratch.py
+++ kmeans_scratch.py
@@ -1,29 +1,59 @@
+
 import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans as SKKMeans
+import matplotlib.pyplot as plt
 
 class KMeansScratch:
-    def __init__(self, k=3, max_iters=300):
+    def __init__(self, k, max_iter=300, tol=1e-4):
         self.k = k
-        self.max_iters = max_iters
+        self.max_iter = max_iter
+        self.tol = tol
 
     def fit(self, X):
-        n, d = X.shape
-        rng = np.random.default_rng(42)
-        self.centroids = X[rng.choice(n, self.k, replace=False)]
+        np.random.seed(42)
+        idx = np.random.choice(len(X), self.k, replace=False)
+        self.centroids = X[idx]
 
-        for _ in range(self.max_iters):
-            distances = np.linalg.norm(X[:, None] - self.centroids[None, :], axis=2)
-            labels = np.argmin(distances, axis=1)
-            new_centroids = []
-            for i in range(self.k):
-                pts = X[labels == i]
-                if len(pts) > 0:
-                    new_centroids.append(pts.mean(axis=0))
-                else:
-                    new_centroids.append(self.centroids[i])
-            new_centroids = np.array(new_centroids)
-
-            if np.allclose(self.centroids, new_centroids):
+        for _ in range(self.max_iter):
+            labels = self._assign_clusters(X)
+            new_centroids = self._compute_centroids(X, labels)
+            if np.linalg.norm(self.centroids - new_centroids) < self.tol:
                 break
             self.centroids = new_centroids
 
-        self.labels_ = labels
+        self.labels_ = self._assign_clusters(X)
+        self.inertia_ = self._compute_inertia(X, self.labels_)
+
+    def _assign_clusters(self, X):
+        distances = np.linalg.norm(X[:, None] - self.centroids, axis=2)
+        return np.argmin(distances, axis=1)
+
+    def _compute_centroids(self, X, labels):
+        return np.array([X[labels == i].mean(axis=0) for i in range(self.k)])
+
+    def _compute_inertia(self, X, labels):
+        return sum(np.sum((X[labels == i] - self.centroids[i])**2) for i in range(self.k))
+
+if __name__ == "__main__":
+    X, y_true = make_blobs(n_samples=600, centers=4, cluster_std=0.6, random_state=42)
+
+    inertias = []
+    for k in range(2, 11):
+        km = KMeansScratch(k)
+        km.fit(X)
+        inertias.append(km.inertia_)
+
+    optimal_k = np.argmin(np.gradient(np.gradient(inertias))) + 2
+
+    final_model = KMeansScratch(optimal_k)
+    final_model.fit(X)
+    sil_scratch = silhouette_score(X, final_model.labels_)
+
+    sk = SKKMeans(n_clusters=optimal_k, random_state=42).fit(X)
+    sil_sklearn = silhouette_score(X, sk.labels_)
+
+    print("Optimal K:", optimal_k)
+    print("Silhouette (scratch):", sil_scratch)
+    print("Silhouette (sklearn):", sil_sklearn)
